16ef758fb4673e9aa2c81a06eb74f439e6ea41b9
bmcilhenny <bmcilhen@gmail.com>
2019-06-21 00:24:32 -0400

    count number of herok/router occurences as total pages served as that is the number of pages served, not # of lines

--- parser.rb
+++ parser.rb
@@ -5,49 +5,50 @@ lines = File.readlines './2014-09-03.log'
 
 missing_errors_hash_map = Hash.new(0)
 db_tables_hash_map = Hash.new(0)
-length = lines.length
-total_serv = 0.0
-
-def four_o_four_hash_map(line)
-    url = line.scan(/path=\"(.*?)\"/).first
-    url = line.scan(/host=([^\s]+)/)
-    missing_errors[url] += 1 unless url.empty?
-end
-
+# length = 0
+time_served = 0.0
+pages_served = 0
 heroku_routers = 0
 total_sql = 0
 
 lines.each_with_index do |line, idx|
+    
     # 1 - hash map 404_urls
     missing_error = line.match?(/status=404/)
     if missing_error
         path = line.scan(/path=\"(.*?)\"/).flatten.first
         host = line.scan(/host=([^\s]+)/).flatten.first
-        missing_errors[host + path] += 1 unless path.nil? || host.nil?
+        missing_errors_hash_map[host + path] += 1 unless path.nil? || host.nil?
     end
-    server errors
-    server_err = line.match?(/status=5\d\d/)
 
-    # 2 - sum total services
+    # 2 - sum total services ( not including connect time)
     service = line.scan(/service=(\d+)ms/).flatten.first
-    total_serv += service.to_i unless service.nil?
+    # connect = line.scan(/connect=(\d+)ms/).flatten.first
+    page_was_served = line.match?(/heroku\/router/)
+    time_served += service.to_i unless service.nil?
+    # time_served += (service.to_i + connect.to_i) unless service.nil?
+    pages_served += 1 if page_was_served
+    # length += 1 if heroku_router_match
 
     # 3 - most loaded db
-    db_table = line.scan(/FROM \"(.*?)"/).flatten
+    db_table = line.scan(/FROM \"(.*?)"|JOIN \"(.*?)"/).flatten.compact
+    # db_table += line.scan(/JOIN \"(.*?)"/).flatten
     db_table.each{|db_table| db_tables_hash_map[db_table] += 1}
-    heroku_router = line.match?(/heroku\/router/)
-    if heroku_router
-        heroku_routers += 1 if heroku_router
-        has_sql = line.match?(/SELECT/)
-        total_sql += 1 if has_sql
-    end
+
+    # used this line to check if line had heroku/router
+    # heroku_router = line.match?(/heroku\/router/)
+    # if heroku_router
+    #     heroku_routers += 1 if heroku_router
+    #     has_sql = line.match?(/SELECT/)
+    #     total_sql += 1 if has_sql
+    # end
 
 end
 
-avg_serv = (total_serv/length).round(2)
+avg_serv = (time_served/pages_served).round(2)
 most_common_db_table = db_tables_hash_map.max_by{|k,v| v}.first
 
 
-puts missing_errors_hash_map
-puts avg_serv
-puts most_common_db_table
+puts "404 url error breakdown", missing_errors_hash_map
+puts "Avg time took to serv (ms)", avg_serv
+puts "most common Database table loaded", most_common_db_table
